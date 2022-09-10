bbd133c351c77f50673e7c5b5f661079548303e9
MrAx <110979574+MrAax@users.noreply.github.com>
2022-09-10 17:02:54 +0530

    Update ping_all_local_ip.py

--- ping_all_local_ip.py
+++ ping_all_local_ip.py
@@ -2,6 +2,9 @@ import subprocess
 import platform
 import os
 import time
+#import urllib2
+from urllib.request import urlopen
+import json
 
 def ping(host):
 	global parem
@@ -9,20 +12,19 @@ def ping(host):
 	command = ['ping', parem, '2', host]
 	return subprocess.call(command)
 '''
-for x in range(0, 256, 1):
+for i in range(0, 256, 1):
   for j in range(0, 256, 1):
-  	#print(f'ping : 192.168.{x}.{j}')
-  	host = f'192.168.{x}.{j}'
+  	#print(f'ping : 192.168.{i}.{j}')
+  	host = f'192.168.{i}.{j}'
   	print(ping(host))
 '''
 
 '''
 while True:
-	#host = 'gmail.com'
+	#host = 'google.com'
 	host = str(input('Enter website name : '))
 	print(ping(host))
 '''
-
 
 
 #	give ip list in ip-source.txt file
@@ -30,10 +32,20 @@ with open('ip-source.txt') as file:
 	dump = file.read()
 	dump = dump.splitlines()
 	for ip in dump:
-		os.system('clear')
-		print('ping new', ip)
-		print('_'*60)
-		os.system('ping -c 4 {}'.format(ip))
-		print('_'*60)
-		time.sleep(5)
+		if (platform.system().lower() == 'windows'):
+			os.system('cls')
+			print('Ping new ip - ', ip)
+			print('_' * 60)
+			os.system('ping -n 2 {}'.format(ip))
+			print('_' * 60)
+			time.sleep(5)
+		else:
+			os.system('clear')
+			print('Ping new ip - ', ip)
+			print('_' * 60)
+			#os.system('ping -c 4 {}'.format(ip))
+			online=os.system(f'ping -c 2 {ip}')
+			print('_' * 60)
+			time.sleep(5)
+			
 		
